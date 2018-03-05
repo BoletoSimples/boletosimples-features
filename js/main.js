7c1ede2f351d94568d32b5cf2d1cb1e811039085
Rafael Lima <contato@rafael.adm.br>
2018-03-05 17:16:30 -0300

    updates header and footer

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 Main = {
   init: function(){
-
-
-    // mobile menu
-
     $("#user_email").focus();
-
-
-    /* mobile nav */
-
-    $("#menu-trigger").click(function() {
-      $("#main-header nav ul").toggle();
-      $(".header-form").hide();
-    });
-
   }
 }
 
